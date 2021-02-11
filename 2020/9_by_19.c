38b5243c5d68970709fe4d4e886286c1a1c81537
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-02-11 23:25:49 +0900

    Create 9_by_19.c

---
+++ 2020/9_by_19.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int h = 9, w = 19;
+  int * * p = malloc(sizeof(*p) * h + sizeof(**p) * h * w);
+  for(int i=0; i<h; i++)
+  {
+    p[i] = (char *)(p+h)+sizeof **p * i * w;
+  }
+  
+  for(int i=0;i<h;i++)
+  {
+    for(int j=0;j<w;j++)
+    {
+      p[i][j] = (i+1)*(j+1);
+    }
+  }
+  for(int i=0;i<h;i++)
+  {
+    for(int j=0;j<w;j++)
+    {
+      printf("%4i", p[i][j]);
+    }
+    putchar(10);
+  }
+  return 0;
+}
