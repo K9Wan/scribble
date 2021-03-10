cf687a5c3f200aec42f57d17390fcc60680f083a
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-03-10 23:18:54 +0900

    Update prime.py

--- 2020/prime.py
+++ 2020/prime.py
@@ -9,3 +9,18 @@ def is_prime(n):
         if n%i == 0:
             return False
     return True
+
+def prime1(n):
+    primes = []
+    if n>=2:
+        primes.append(2)
+    if n>=3:
+        primes.append(3)
+    for i in range(5,n+1,2):
+        isprime=True
+        for p in primes:
+            if i%p == 0:
+                isprime=False
+        if isprime:
+            primes.append(i)
+    yield from primes
