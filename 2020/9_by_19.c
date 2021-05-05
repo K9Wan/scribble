9c245744da17592b3d69f78aaf392c5a080c2bb2
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-05-05 23:11:12 +0900

    Update 9_by_19.c

--- 2020/9_by_19.c
+++ 2020/9_by_19.c
@@ -5,10 +5,26 @@ int main(void)
 {
   int h = 9, w = 19;
   int * * p = malloc(sizeof(*p) * h + sizeof(**p) * h * w);
+
+  p[0] = (void *)(p+h);
+  for(int i=1; i<h; i++)
+  {
+    p[i] = p[i-1] + w;
+  }// from comment on https://panty.run/malloc/ by 은하
+
+  /*/
   for(int i=0; i<h; i++)
   {
     p[i] = (void *)((char *)(p+h)+sizeof **p * i * w);
-  }
+  }// my original
+  /*/
+
+  /*/
+  for(int i=0; i<h; i++)
+  {
+    p[i] = (int *)(p+h)+ i * w;
+  }// my old original: short but hardcoded typecasting
+  /*/
   
   for(int i=0;i<h;i++)
   {
