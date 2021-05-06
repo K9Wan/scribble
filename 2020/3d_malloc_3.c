86fb0e88fc98da641f628cd1402acaa2830e8c94
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-05-06 23:29:03 +0900

    Create 3d_malloc_3.c

---
+++ 2020/3d_malloc_3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int x = 8, y = 7, z = 9;
+  int *** a = malloc(sizeof *a * x + sizeof **a * x * y + sizeof ***a * x * y * z);
+  for(int i=0; i<x; i++)
+  {
+    a[i] = (void *)((char *)(a+x) + sizeof **a*i*y);
+  }
+  for(int i=0; i<x; i++)
+  {
+    a[i][0] = (void *)((char *)(a[0] + x*y) + sizeof ***a*i*z);
+  }
+  for(int i=0; i<x; i++)
+  {
+    for(int j=0; j<y; j++)
+    {
+      a[i][j] = a[i][0] + j*x*z;
+    }
+  }
+
+  for(int i=0; i<x; i++)
+  {
+    for(int j=0; j<y; j++)
+    {
+      for(int k=0; k<z; k++)
+      {
+        a[i][j][k] = (i+1)*100+(j+1)*10+(k+1);
+      }
+    }
+  }
+  
+  for(int i=0; i<x; i++)
+  {
+    for(int j=0; j<y; j++)
+    {
+      for(int k=0; k<z; k++)
+      {
+        printf("%4i", a[i][j][k]);
+      }
+      putchar(10);
+    }
+    putchar(10);
+  }
+
+  free(a);
+  
+  return 0;
+}
