798e29dc30ebcaf3ed27ed7dac9c55906f3eacd5
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-05-07 23:36:36 +0900

    Update bst2.py

--- 2020/bst2.py
+++ 2020/bst2.py
@@ -1,40 +1,86 @@
+# help from https://stackoverflow.com/questions/67285277/recursive-class-definition-in-python
 class Node(object):
     def __init__(self, key=None, data=None):
         self.key = key
         self.data = data
 
+    def noname1(self):
+        '''
+        can't find appropriate name:
+        get derive gain take generate yield obtain take return ...
+        value data pair ...
+        adaptable flexible custom ...
+        '''
+        if self.data is None:
+            return self.key
+        return (self.key, self.data)
+
+    def _noname2(self):
+        yield self.key
+        if self.data is not None:
+            yield self.data
+
+    def noname2(self):
+        return tuple(self._noname2())
+
+    
 
 class BinarySearchTree(object):
-    def __init__(self, node=None, keyfunc=lambda x: x):
+    def __init__(self, keyfunc=lambda x: x, *, node=None):
         self.root = node
         self.keyfunc = keyfunc
         if node is not None:
-            self.left = __class__(keyfunc=self.keyfunc)
-            self.right = __class__(keyfunc=self.keyfunc)
+            self.left = self.new_empty()
+            self.right = self.new_empty()
+
+    def __bool__(self):
+        return self.root is not None
+
+    def new_empty(self):
+        return __class__(keyfunc=self.keyfunc)
 
     def add(self, key, data=None):
         node = Node(key, data)
-        if self.root is None:
+        if not self:
             self.root = node
-            self.left = __class__(keyfunc=self.keyfunc)
-            self.right = __class__(keyfunc=self.keyfunc)
+            self.left = self.new_empty()
+            self.right = self.new_empty()
             return
         parent = self.root.key
         if self.keyfunc(key) < self.keyfunc(parent):
             self.left.add(key, data)
         elif self.keyfunc(key) > self.keyfunc(parent):
             self.right.add(key, data)
+        '''
+        else: pass will ignore duplicates,
+        else: self.right.add(...) will accept duplicates.
+        (sure can replace elif above with else, or replace > with >=)
+        or can consider it as wrong
+        '''
+
+    def print_inorder(self):
+        if self:
+            self.left.print_inorder()
+            print(self.root.key, end=" ")
+            self.right.print_inorder()
 
     def inorder(self):
-        if self.root:
-            self.left.inorder()
-            print(self.root.key, end=" ")
-            self.right.inorder()
+        if self:
+            yield from self.left.inorder()
+            yield self.root.noname1()
+            yield from self.right.inorder()
+
+    def preorder(self): pass
+
+    def postorder(self): pass
+
+    
 
 
 if __name__ == "__main__":
-    bst1 = BinarySearchTree()
+    bst1 = BinarySearchTree(lambda x: -x)
     arr = [2, 6, 4, 3, 2, 7, 8, 1, 9, 5]
     for key in arr:
         bst1.add(key)
-    bst1.inorder()
+    bst1.print_inorder()
+    print([*bst1.inorder()])
