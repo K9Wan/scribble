f8ab62488ed09b9274da1a79a6269e5851fed1c1
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-06-07 23:57:23 +0900

    Create inf.py

---
+++ 2020/inf.py
@@ -0,0 +1,8 @@
+class Infinity(object):
+	inf = None
+	def __new__(cls, *args, **kwargs):
+		if cls.inf is None:
+			cls.inf = super().__new__(cls, *args, **kwargs)
+		return cls.inf
+	def __gt__(self, other):
+		return True
