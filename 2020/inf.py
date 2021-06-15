fb160cb8b1b6fbabb1c3f7ad90ac2b2e3c72d3e2
K9Wan <40694383+K9Wan@users.noreply.github.com>
2021-06-15 22:24:05 +0900

    Update inf.py

--- 2020/inf.py
+++ 2020/inf.py
@@ -1,10 +1,10 @@
 class Infinity(object):
-	inf = None
-	def __new__(cls, *args, **kwargs):
-		if cls.inf is None:
-			cls.inf = super().__new__(cls, *args, **kwargs)
-		return cls.inf
-	def __gt__(self, other):
-		return True
-	
-	pass
+    inf = None
+    def __new__(cls, *args, **kwargs):
+        if cls.inf is None:
+            cls.inf = super().__new__(cls, *args, **kwargs)
+        return cls.inf
+    def __gt__(self, other):
+        return True
+    
+    pass
